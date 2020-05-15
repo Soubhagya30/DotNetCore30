8b92c6851ed6b1bfdc0d8ff5ad9d6a8399403e86
Soubhagya30 <soubhagyalaxmi.g@razorpay.com>
2020-05-15 15:35:56 +0530

    core

---
+++ dotnetCore30/Pages/Charge.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Razorpay.Api;
+
+namespace dotnetCore30
+{
+    public class ChargeModel : PageModel
+    {
+        //protected void Page_Load(object sender, EventArgs e)
+        public void OnGet()
+        {
+
+            string paymentId = Request.Form["razorpay_payment_id"];
+
+                Dictionary<string, object> input = new Dictionary<string, object>();
+                input.Add("amount", 100); // this amount should be same as transaction amount
+
+                string key = "rzp_test_2pjQoIV7c1RY6C";
+                string secret = "nWwe91xQO3NIDzJUp1mUmr9O";
+
+
+                RazorpayClient client = new RazorpayClient(key, secret);
+
+                Dictionary<string, string> attributes = new Dictionary<string, string>();
+
+                attributes.Add("razorpay_payment_id", paymentId);
+                attributes.Add("razorpay_order_id", Request.Form["razorpay_order_id"]);
+                attributes.Add("razorpay_signature", Request.Form["razorpay_signature"]);
+
+                Utils.verifyPaymentSignature(attributes);
+
+                //Refund refund = new Razorpay.Api.Payment((string)paymentId).Refund();
+
+                //Console.WriteLine(refund["id"]);
+            }
+        }
+    }
+    
---
+++ dotnetCore30/Pages/Payment.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Razorpay.Api;
+
+namespace dotnetCore30
+{
+    public class PaymentModel : PageModel
+    {
+        public string orderId;
+
+        //protected void Page_Load(object sender, EventArgs e)
+        public void OnGet()
+        {
+            Dictionary<string, object> input = new Dictionary<string, object>();
+            input.Add("amount", 100); // this amount should be same as transaction amount
+            input.Add("currency", "INR");//
+            input.Add("receipt", "12121");
+            input.Add("payment_capture", 1);
+
+            string key = "rzp_test_2pjQoIV7c1RY6C";
+            string secret = "nWwe91xQO3NIDzJUp1mUmr9O";
+
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
+
+            RazorpayClient client = new RazorpayClient(key, secret);
+
+            Razorpay.Api.Order order = client.Order.Create(input);
+            orderId = order["id"].ToString();
+
+
+        }
+    }
+}
